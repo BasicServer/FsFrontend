3c7d68508c4c74e4c48171db3ae17c66843ac702
vd <viridian035@gmail.com>
2023-07-24 00:12:47 +0200

    fix cleaning

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 function cleanPath(path: string): string {
-	return path.replace(/^\/\//g, '/');	
+	return path.replace(/^\/\//g, '');
 }
 
 export async function readdir(path: string) {
@@ -22,59 +22,49 @@ export async function readFile(path: string) {
 
 export async function mkdir(path: string) {
 	path = cleanPath(path);
-	return await (
-		await fetch(`/dir/${path}`, {
-			method: 'PUT',
-		})
-	).text();
+	await fetch(`/dir/${path}`, {
+		method: 'PUT',
+	});
 }
 
 export async function writeFile(path: string, content: string) {
 	path = cleanPath(path);
-	return await (
-		await fetch(`/file/${path}`, {
-			method: 'PUT',
-			body: JSON.stringify({ content }),
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
-	).text();
+	await fetch(`/file/${path}`, {
+		method: 'PUT',
+		body: JSON.stringify({ content }),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
 }
 
 export async function rm(path: string) {
 	path = cleanPath(path);
-	return await (
-		await fetch(`/file/${path}`, {
-			method: 'DELETE',
-		})
-	).text();
+	await fetch(`/file/${path}`, {
+		method: 'DELETE',
+	});
 }
 
 export async function cp(src: string, dest: string) {
 	src = cleanPath(src);
 	dest = cleanPath(dest);
-	return await (
-		await fetch(`/copyfile`, {
-			method: 'POST',
-			body: JSON.stringify({ src, dest}),
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
-	).text();
+	await fetch(`/copyfile`, {
+		method: 'POST',
+		body: JSON.stringify({ src, dest }),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
 }
 
 export async function rename(src: string, dest: string) {
 	src = cleanPath(src);
 	dest = cleanPath(dest);
-	return await (
-		await fetch(`/movefile`, {
-			method: 'POST',
-			body: JSON.stringify({ src, dest}),
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
-	).text();
+	await fetch(`/movefile`, {
+		method: 'POST',
+		body: JSON.stringify({ src, dest }),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
 }
